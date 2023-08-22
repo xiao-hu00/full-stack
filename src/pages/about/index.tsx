c279a043cf15442ac5a2872aa8a4f6bdb3c1dd32
wanli <406659545@qq.com>
2023-08-22 17:28:54 +0800

    init

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -3,14 +3,30 @@ import { Canvas, useLoader } from '@react-three/fiber'
 import threePx from '@/assets/3px.png'
 import { TextureLoader } from 'three/src/loaders/TextureLoader.js'
 import * as THREE from 'three'
+import { useScroll } from 'ahooks'
 
 const Component: React.FC = () => {
   const threeMap: THREE.Texture = useLoader(TextureLoader, threePx)
   threeMap.magFilter = THREE.NearestFilter
-  // const 
+  const scroll = useScroll(document) as {
+    top: number
+    left: number
+  }
+  const wh = window.innerHeight
+  const positionY = scroll?.top > 0 ? (scroll?.top / wh) * 8 : 0
+  // const mouse = useMouse()
+  // const cx = mouse.screenX / window.innerWidth
+  // const cy = mouse.screenY / window.innerHeight
   return (
     <div style={{ height: '100%', overflow: 'scroll' }}>
-      <div style={{ position: 'absolute', background: '#000', height: 'calc(100vh-42px)' , width: '100%', top: 42 }}>
+      <div
+        style={{
+          position: 'absolute',
+          background: '#000',
+          width: '100%',
+          top: 42,
+        }}
+      >
         <div style={{ color: '#FFF' }} className='h-[100vh] text-3xl'>
           HELLO
         </div>
@@ -21,25 +37,41 @@ const Component: React.FC = () => {
           HELLO
         </div>
       </div>
-      <Canvas style={{ position: 'fixed', width: '100%', height: '100%', top: 42, zIndex: 1 }}>
+      <Canvas
+        camera={{
+          fov: 30,
+          near: 0.1,
+          far: 100,
+          zoom: 0.3,
+        }}
+        style={{
+          position: 'fixed',
+          width: '100%',
+          height: '100%',
+          top: 42,
+          zIndex: 1,
+        }}
+      >
         <ambientLight intensity={1} />
         <directionalLight position={[1, 0, 0]} />
-        <mesh position={[0, -2, 0]}>
-          <torusGeometry args={[1, 0.25, 6, 80]} />
-          <meshToonMaterial
-            gradientMap={threeMap}
-            transparent={true}
-            color={'hotpink'}
-          />
-        </mesh>
-        <mesh position={[0, 12, 0]}>
-          <torusGeometry args={[1, 0.25, 6, 80]} />
-          <meshToonMaterial
-            gradientMap={threeMap}
-            transparent={true}
-            color={'hotpink'}
-          />
-        </mesh>
+        <group position={[0, positionY, 0]}>
+          <mesh position={[0, 2, 0]}>
+            <torusGeometry args={[1, 0.25, 6, 80]} />
+            <meshToonMaterial
+              gradientMap={threeMap}
+              transparent={true}
+              color={'hotpink'}
+            />
+          </mesh>
+          <mesh position={[0, -6, 0]}>
+            <torusGeometry args={[1, 0.25, 6, 80]} />
+            <meshToonMaterial
+              gradientMap={threeMap}
+              transparent={true}
+              color={'hotpink'}
+            />
+          </mesh>
+        </group>
       </Canvas>
     </div>
   )
