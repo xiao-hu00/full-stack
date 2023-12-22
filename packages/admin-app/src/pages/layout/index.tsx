67a82daf6bc8ab9f104894f864069b8e42f69019
wanli <406659545@qq.com>
2023-12-22 14:01:15 +0800

    admin

--- packages/admin-app/src/pages/layout/index.tsx
+++ packages/admin-app/src/pages/layout/index.tsx
@@ -1,9 +1,11 @@
-
+import { ModeToggle } from '@/components/mode-toggle'
 
 function Layout() {
 
   return (
     <div>
+      <ModeToggle>
+      </ModeToggle>
       <div>side</div>
       <div>header</div>
       <div>main</div>
