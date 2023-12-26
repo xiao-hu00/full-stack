c174de21522efb623e544d1b095dc145b7c8930f
xiao-hu00 <406659545@qq.com>
2023-12-26 21:20:03 +0800

    admin

--- packages/admin-app/src/pages/layout/sideMenu.tsx
+++ packages/admin-app/src/pages/layout/sideMenu.tsx
@@ -1,22 +1,26 @@
 import { animated, useSpring } from '@react-spring/web'
+import React from 'react'
 
 const SideMenu = () => {
   const [hStyle, api] = useSpring(
     () => ({
-      from: { height: 0 },
-      to: { height: 100 },
+      height: 0
     }),
     []
   )
-  const clickMenu = () => {
-    
+  const clickMenu = (e: React.MouseEvent<HTMLElement>) => {
+    const doms = e.currentTarget.nextElementSibling?.children[0]
+    const dom = e.currentTarget.nextElementSibling
+    const h = dom?.clientHeight ? 0 : (dom?.childElementCount || 0) * ((doms?.clientHeight || 0) + 16 * 0.5)
+    console.log(h)
+    api.start({ height: h })
   }
   return (
     <div className="p-3 dark:bg-slate-900 h-[100vh] overflow-y-auto">
       <div>
         <div onClick={clickMenu} className="w-[100%] py-2 pl-2 rounded cursor-pointer dark:hover:bg-slate-700 hover:bg-gray-100 mb-2">111</div>
-        <animated.div style={hStyle}>
-          <div className="w-[100%] py-2 pl-4 rounded cursor-pointer dark:hover:bg-slate-700 hover:bg-gray-100 mb-2">111-111</div>
+        <animated.div className="menu-child" style={{ ...hStyle, overflow: 'hidden' }}>
+          <div className="menu-item w-[100%] py-2 pl-4 rounded cursor-pointer dark:hover:bg-slate-700 hover:bg-gray-100 mb-2">111-111</div>
           <div className="w-[100%] py-2 pl-4 rounded cursor-pointer dark:hover:bg-slate-700 hover:bg-gray-100 mb-2">111-222</div>
         </animated.div>
       </div>
