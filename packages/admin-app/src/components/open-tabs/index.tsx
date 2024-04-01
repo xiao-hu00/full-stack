a2a9eb9ed56a64467a736f5e8d55af559a3b4db9
wanli <406659545@qq.com>
2024-04-01 19:28:05 +0800

    color

--- packages/admin-app/src/components/open-tabs/index.tsx
+++ packages/admin-app/src/components/open-tabs/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
 import { Separator } from '@/components/ui/separator'
 import { X } from 'lucide-react'
@@ -10,15 +11,26 @@ import {
 
 const OpenTabs = () => {
   const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  const [activeMenu, setActiveMenu] = useState(1)
+  const clickMenu = (item: number) => {
+    setActiveMenu(item)
+  }
   return (
     <ScrollArea className='flex-1 px-4'>
       <div className='flex space-x-4 h-14'>
         {list.map((item, index) => (
           <div key={item} className='flex space-x-2 items-center'>
             <div className='flex items-center space-x-4'>
-              <div className='cursor-pointer whitespace-nowrap hover:text-[hsl(var(--primary))]'>
+              <div className='cursor-pointer whitespace-nowrap hover:text-[hsl(var(--primary)/0.75)]'>
                 <ContextMenu>
-                  <ContextMenuTrigger>menu-{item}</ContextMenuTrigger>
+                  <ContextMenuTrigger
+                    className={`${
+                      activeMenu === item ? 'text-[hsl(var(--primary))]' : ''
+                    }`}
+                    onClick={() => clickMenu(item)}
+                  >
+                    menu-{item}
+                  </ContextMenuTrigger>
                   <ContextMenuContent>
                     <ContextMenuItem>关闭右侧</ContextMenuItem>
                     <ContextMenuItem>关闭左侧</ContextMenuItem>
