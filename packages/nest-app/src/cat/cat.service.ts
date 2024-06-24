8a59d86e0edcc3122ba5a0d87573b356392fa7e6
xiao-hu00 <406659545@qq.com>
2024-06-25 01:32:57 +0800

    read file

--- packages/nest-app/src/cat/cat.service.ts
+++ packages/nest-app/src/cat/cat.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
 import prisma from '@/prisma/client'
+import xlsx from 'node-xlsx';
 
 @Injectable()
 export class CatService {
@@ -11,6 +12,8 @@ export class CatService {
   }
 
   async findAll() {
+    const workSheetsFromFile = xlsx.parse(`./assets/pig1-12/第1季第1集泥坑-中英台词.xlsx`);
+    console.log(workSheetsFromFile?.[0]?.data[1])
     const data = await prisma.cat.findMany()
     return data;
   }
